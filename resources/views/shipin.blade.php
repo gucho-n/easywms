e5d2fe89cd6450147f9a6372b909ce503568a62f
github user name <i.m.your.dog.1192@gmail.com>
2021-08-11 08:34:44 +0900

    入庫画面

--- resources/views/shipin.blade.php
+++ resources/views/shipin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="shipin.css">
+    <link rel="stylesheet" href="{{ asset('css/shipin.css') }}">
     <title>入庫登録</title>
 
 </head>
@@ -12,13 +12,14 @@
     <header>
         <div class="header-name">
           <p>〇〇さん</p>
-        <div>
+        </div>
         <div class=header-auth>
           <p>権限〇〇</p>
         </div>
         <div class=header-logout>
         　<p>ログアウト</p>
         </div>
+    </header>
     <main>
     <div class="title-container">
         <p>入庫登録</p>
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,13 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('auth/login');
 // });
+
+// Route::get('/', function () {
+//     return view('side');
+// });
+
 Route::get('/', function () {
-    return view('side');
+    return view('shipin');
 });
 
 Auth::routes();
