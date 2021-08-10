5a0b0252cdeb76b3b1229e1dd9370d3412176140
github user name <i.m.your.dog.1192@gmail.com>
2021-08-10 20:21:21 +0900

    メインページ雛形

--- resources/views/shipin.blade.php
+++ resources/views/shipin.blade.php
@@ -4,10 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>shipin</title>
+    <link rel="stylesheet" href="shipin.css">
+    <title>入庫登録</title>
+
 </head>
 <body>
-    <p>test</p>
-    <p>test</p>
+    <header>
+        <div class="header-name">
+          <p>〇〇さん</p>
+        <div>
+        <div class=header-auth>
+          <p>権限〇〇</p>
+        </div>
+        <div class=header-logout>
+        　<p>ログアウト</p>
+        </div>
+    </header>
+    <article>
+        <ul>
+            <li>入庫登録</li>
+            <li>出庫引当</li>
+            <li>在庫移動</li>
+            <li>指示書出力</li>
+            <li>ロケーション登録</li>
+            <li>ユーザー登録</li>
+        </ul>
+    </article>
+    <main>
+        <p></p>
+    </main>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('auth/login');
+// });
 Route::get('/', function () {
-    return view('auth/login');
+    return view('shipin');
 });
 
 Auth::routes();
